0a1f4cc4a80f3fb55c1abe77e418e826a6e6255f
Naro <n.justinaro@gmail.com>
2022-02-16 16:24:04 -0500

    Added a slider that shows the value of the current position in a range of 0-64. Next step is to have the grid size reflect the value.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.getElementById('container'); 
 
 
-function gridSize(rows,cols){
+function gridSize(cols,rows){
     container.style.setProperty("--grid-cols", cols);
     container.style.setProperty("--grid-rows", rows);
     for ( let c = 0; c < (cols*rows); c++){
@@ -10,4 +10,15 @@ function gridSize(rows,cols){
     };
 };
 
-gridSize(16,16);
+const slide = document.getElementById('slider');
+let output = document.getElementById('size');
+output.innerHTML = slide.value; 
+ 
+let gridCount = slide.value;
+slide.oninput = function () {
+    output.innerHTML = this.value;
+    return output.innerHTML;
+}
+
+gridSize(gridCount,gridCount);
+
