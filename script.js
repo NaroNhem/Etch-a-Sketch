d7600bab39dfa2027474637b992aa15288b791ff
Naro <n.justinaro@gmail.com>
2022-02-18 14:45:14 -0500

    Finished all functional pieces for the project. Website is working as intended

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
 const container = document.getElementById('container'); 
 
 
+
 function gridSize(cols,rows){
     container.style.setProperty("--grid-cols", cols);
     container.style.setProperty("--grid-rows", rows);
-    for ( let c = 0; c < (cols*rows); c++){
+    for ( let c = 0; c <= (cols*rows); c++){
         let cell = document.createElement("div");
         container.appendChild(cell).className = "grid-item";
+        cell.addEventListener("mouseover", mouseOver);
+        function mouseOver(event) {
+            let colorpicker = document.getElementById('colorpicker');
+            let color = colorpicker.value;
+            cell.style.backgroundColor = color;
+        }
+        if (c == 0 ){
+            document.getElementById('container').innerHTML = "";
+        }
     };
 };
 
 const slide = document.getElementById('slider');
 let output = document.getElementById('size');
 output.innerHTML = slide.value; 
- 
-let gridCount = slide.value;
+
+slide.onclick = gridSize(output.innerHTML, output.innerHTML)
+
 slide.oninput = function () {
     output.innerHTML = this.value;
-    return output.innerHTML;
+    let x = output.innerHTML;
+    gridSize(x,x);
 }
 
-gridSize(gridCount,gridCount);
+
+function clearCanvas() {
+    document.getElementById('container').innerHTML = "";
+    slide.onclick = gridSize(output.innerHTML, output.innerHTML)
+}
+
+
+
+
+
+
+ 
+
 
